59246e1b36990948b6c439c91a4a934c954c8af5
Jack <jack.70aw@gmail.com>
2024-09-09 20:54:50 +0330

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import DarkNavbar from "./components/DarkNavbar"
+
+export default function Home() {
+  return (
+    <div>
+      <DarkNavbar/>
+      <p>Is there any point in life or just should we kill </p>
+    </div>
+  );
+}
