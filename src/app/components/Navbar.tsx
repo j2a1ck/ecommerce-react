ce58fc247488d36dfed1e0c55eccc41bb757017e
Jack <jack.70aw@gmail.com>
2024-09-12 18:43:37 +0330

    combine navbar

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import DarkNavbar from "./DarkNavbar"
+import LightNavbar from "./LightNavbar"
+
+const Navbar = () => {
+    return (
+      <div>
+        <DarkNavbar />
+        <LightNavbar />
+      </div>
+    );
+}
+
+export default Navbar;
