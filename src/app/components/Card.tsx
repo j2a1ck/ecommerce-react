2beba8a3fae91691bc11ae4e0573dd1e7b420764
Jack <jack.70aw@gmail.com>
2024-09-16 19:56:35 +0330

    feat: switch between card pic

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,8 +1,31 @@
+"use client";
+
 import { IoIosArrowForward } from "react-icons/io";
+import ladyImage from "../../public/images/lady.png";
+import shockImage from "../../public/images/shock.jpg";
+import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
+import { useState } from "react";
+
 const Card = () => {
+  const [imageIndex, setImageIndex] = useState(false);
+  const [backgroundImage, setBackgroundImage] = useState(ladyImage.src);
+
+  const handleArrowIcon = () => {
+    const newImageIndex = !imageIndex;
+    setImageIndex(newImageIndex);
+    if (newImageIndex) {
+      setBackgroundImage(shockImage.src);
+    } else {
+      setBackgroundImage(ladyImage.src);
+    }
+  };
+
   return (
-    <div className="lg:flex hidden  bg-[url('../app/assets/lady.png')] h-[600px]">
-      <div className="text-white font-bold p-3 justify-center  flex  flex-col pl-52 ">
+    <div
+      className="lg:flex hidden min-h-screen bg-cover bg-center bg-no-repeat relative"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <div className="text-gray-800 font-bold p-3 justify-center flex flex-col pl-52">
         <h4 className="text-1xl">SUMME 2024</h4>
         <h1 className="mt-20 text-6xl">NEW COLLECTION</h1>
         <div className="mt-20 text-lg font-normal ">
@@ -16,8 +39,34 @@ const Card = () => {
           </button>
         </div>
       </div>
-      <button className="flex  flex-grow justify-end items-center m-5 ">
-        <IoIosArrowForward size={60} className="fill-white" />
+      <div className="flex items-end m-4 ">
+        {imageIndex ? (
+          <>
+            <div className="w-10 h-2 bg-slate-400"></div>
+            <div className="w-10 h-2 bg-white "></div>
+          </>
+        ) : (
+          <>
+          <div className="w-10 h-2 bg-white "></div>
+          <div className="w-10 h-2 bg-slate-400"></div>
+          </>
+
+          )}
+      </div>
+      <button className="flex flex-grow justify-end items-center m-5">
+        {imageIndex ? (
+          <MdOutlineKeyboardArrowLeft
+            onClick={handleArrowIcon}
+            size={70}
+            className="fill-black"
+          />
+        ) : (
+          <IoIosArrowForward
+            onClick={handleArrowIcon}
+            size={60}
+            className="fill-white"
+          />
+        )}
       </button>
     </div>
   );
