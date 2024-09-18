c0d25d5513047ce6a4597604a88bd9211001bcc6
Jack <jack.70aw@gmail.com>
2024-09-18 14:43:44 +0330

    feat: add CardShop and  restructure

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,79 +1,13 @@
-"use client";
-
-import { motion } from "framer-motion";
-import { IoIosArrowForward } from "react-icons/io";
-import ladyImage from "../../public/images/lady.png";
-import shockImage from "../../public/images/shock.jpg";
-import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
-import { useState } from "react";
+import CardHeader from "./CardHeader"
+import CardShop from "./CardShop"
 
 const Card = () => {
-  const [imageIndex, setImageIndex] = useState(false);
-  const [backgroundImage, setBackgroundImage] = useState(ladyImage.src);
-
-  const handleArrowIcon = () => {
-    const newImageIndex = !imageIndex;
-    setImageIndex(newImageIndex);
-    if (newImageIndex) {
-      setBackgroundImage(shockImage.src);
-    } else {
-      setBackgroundImage(ladyImage.src);
-    }
-  };
-
-  return (
-    <div
-      className="lg:flex hidden min-h-screen bg-cover bg-center bg-no-repeat relative"
-      style={{ backgroundImage: `url(${backgroundImage})` }}
-    >
-      <div className="text-gray-800 font-bold p-3 justify-center flex flex-col pl-52">
-        <h4 className="text-1xl">SUMME 2024</h4>
-        <h1 className="mt-20 text-6xl">NEW COLLECTION</h1>
-        <div className="mt-20 text-lg font-normal ">
-          <p>We know large objects will act.</p>
-          <p>but things on a small scale.</p>
-        </div>
-        {/* TODO add functionality to Button */}
-        <div className="flex">
-          <button className="w-40 h-12 bg-green-500 rounded-md mt-8">
-            SHOP NOW
-          </button>
-        </div>
-      </div>
-      <div className="flex items-end m-4 ">
-        {imageIndex ? (
-          <>
-            <div className="w-10 h-2 bg-slate-400"></div>
-            <div className="w-10 h-2 bg-white "></div>
-          </>
-        ) : (
-          <>
-            <div className="w-10 h-2 bg-white "></div>
-            <div className="w-10 h-2 bg-slate-400"></div>
-          </>
-        )}
-      </div>
-      <button className="flex flex-grow justify-end items-center m-5">
-        {imageIndex ? (
-          <motion.div whileHover={{ scale: 1.5 }}>
-            <MdOutlineKeyboardArrowLeft
-              onClick={handleArrowIcon}
-              size={70}
-              className="fill-black"
-            />
-          </motion.div>
-        ) : (
-          <motion.div whileHover={{ scale: 1.6 }}>
-            <IoIosArrowForward
-              onClick={handleArrowIcon}
-              size={60}
-              className="fill-white"
-            />
-          </motion.div>
-        )}
-      </button>
+  return(
+    <div>
+    <CardHeader/>
+    <CardShop/>
     </div>
-  );
-};
+  )
+}
 
 export default Card;
---
+++ src/app/components/CardShop.tsx
@@ -0,0 +1,63 @@
+import men from "../../public/images/men.png";
+import women from "../../public/images/women.png";
+import accessories from "../../public/images/accessories.png"
+import kids from "../../public/images/kids.png"
+const CardShop = () => {
+  return (
+    <div className="h-[770px] bg-slate-100 hidden md:flex lg:flex flex-col items-center">
+      <div className="flex items-center flex-col">
+        <h1 className="mt-[80px] text-2xl mb-[10px] font-bold">
+          EDITOR’S PICK
+        </h1>
+        <h2 className="text-gray-600 text-lg font-normal">
+          Problems trying to resolve the conflict between{" "}
+        </h2>
+      </div>
+      <div className="flex ">
+        <div
+          className="mt-[48px] h-[500px] w-[500px] bg-cover"
+          style={{ backgroundImage: `url(${men.src})` }}
+        >
+          <div className="flex justify-start items-end h-[500px] p-5 ">
+            <button className="flex ml-[9px] w-20 font-medium justify-center items-center rounded-lg h-10 bg-white  ">
+              Men
+            </button>
+          </div>
+        </div>
+        <div
+          style={{ backgroundImage: `url(${women.src})` }}
+          className="bg-cover w-[240px] mt-[48px] h-[500px] ml-[30px] "
+        >
+          <div className="flex justify-start items-end h-[500px] p-5 ">
+            <button className="flex ml-[9px] w-20 font-medium justify-center items-center rounded-lg h-10 bg-white  ">
+              Women
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-col">
+        <div
+          style={{ backgroundImage: `url(${accessories.src})` }}
+          className="bg-cover w-[240px] mt-[48px] h-[242px] ml-[30px] "
+        >
+          <div className="flex justify-start items-end h-[242px] p-5 ">
+            <button className="flex ml-[9px] w-28 justify-center font-medium items-center rounded-lg h-10 bg-white  ">
+              Accessories
+            </button>
+          </div>
+        </div>
+        <div
+          style={{ backgroundImage: `url(${kids.src})` }}
+          className="bg-cover w-[240px] mt-[16px] h-[242px] ml-[30px] "
+        >
+          <div className="flex justify-start items-end h-[242px] p-5 ">
+            <button className="flex ml-[9px] w-20 font-medium justify-center items-center rounded-lg h-10 bg-white  ">
+              Kids
+            </button>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default CardShop;
