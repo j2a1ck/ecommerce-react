b04a5dc0f0776bc01a5534bbd59457f49ce4da6b
Jack <jack.70aw@gmail.com>
2024-09-19 16:41:40 +0330

    Update Card

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,11 +1,13 @@
 import CardHeader from "./CardHeader"
 import CardShop from "./CardShop"
+import CardProduct from "./CardProduct"
 
 const Card = () => {
   return(
     <div>
     <CardHeader/>
     <CardShop/>
+    <CardProduct/>
     </div>
   )
 }
