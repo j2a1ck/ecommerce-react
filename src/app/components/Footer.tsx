6e949b4db8485b37724ed4a8939ee6c7b9076f36
Jack <jack.70aw@gmail.com>
2024-09-25 12:21:14 +0330

    fix: improve responsiveness for mobile

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,7 @@ import { FaFacebook } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 const Footer = () => {
   return (
-    <div className="lg:flex md:flex flex-col hidden mt-10">
+    <div className="flex flex-col mt-10">
       <div className=" bg-slate-100 ">
         <div className="h-[142] max-w-full flex justify-between items-center m-[40px]">
           <div className="font-extrabold text-2xl font-mono">Bandage</div>
@@ -15,22 +15,22 @@ const Footer = () => {
         </div>
       </div>
       <div>
-        <div className="flex justify-center space-x-28 h-[242px] w-full ">
-          <div className="flex flex-col justify-center h-[262] mt-2 bg-white ">
+        <div className="flex flex-col lg:flex-row  justify-center text-start ml-10 lg:ml-0 lg:space-x-28  lg:h-[242px] w-full">
+          <div className="flex flex-col justify-center mt-10 h-[262] lg:mt-2 bg-white ">
             <h1 className="mb-3 font-bold text-base">Company Info</h1>
             <h2 className="text-gray-400 font-medium text-sm m-1">About Us</h2>
             <h2 className="text-gray-400 font-medium text-sm m-1">Carrier</h2>
             <h2 className="text-gray-400 font-medium text-sm m-1">We are hiring</h2>
             <h2 className="text-gray-400 font-medium text-sm m-1">Blog</h2>
           </div>
-          <div className="flex flex-col justify-center max-w-full  h-[262] mt-2 bg-white ">
+          <div className="lg:flex flex-col justify-center max-w-full hidden h-[262] mt-2 bg-white ">
             <h1 className="mb-3 font-bold text-base">Legal</h1>
             <h2 className="text-gray-400 font-medium text-sm m-1">About Us</h2>
             <h2 className="text-gray-400 font-medium text-sm m-1">Carrier</h2>
             <h2 className="text-gray-400 font-medium text-sm m-1">We are hiring</h2>
             <h2 className="text-gray-400 font-medium text-sm m-1">Blog</h2>
           </div>
-          <div className="flex flex-col justify-center max-w-full  h-[262] mt-2 bg-white">
+          <div className="lg:flex flex-col justify-center max-w-full hidden h-[262] mt-2 bg-white">
             <h1 className="mb-3 font-bold text-base">Features</h1>
             <h2 className="text-gray-400 font-medium text-sm m-1">
               Business Marketing
@@ -41,16 +41,16 @@ const Footer = () => {
               Unlimited Support
             </h2>
           </div>
-          <div className="flex flex-col justify-center max-w-full  h-[262] mt-2 bg-white ">
+          <div className="flex flex-col justify-center max-w-full mt-10  h-[262] lg:mt-2 bg-white ">
             <h1 className="mb-3 font-bold text-base">Resources</h1>
             <h2 className="text-gray-400 font-medium text-sm m-1">IOS & Android</h2>
             <h2 className="text-gray-400 font-medium text-sm m-1">Watch a Demo</h2>
             <h2 className="text-gray-400 font-medium text-sm m-1">Customers</h2>
             <h2 className="text-gray-400 font-medium text-sm m-1">API</h2>
           </div>
-          <div className="flex flex-col justify-center">
+          <div className="flex flex-col mt-10 lg:mt-0 justify-center">
             <div className="mb-3 font-bold text-base">Get In Touch</div>
-            <div className="flex">
+            <div className="flex justify-start mt-4 lg:mt-0 mb-5 lg:mb-1">
               <input
                 placeholder="Your Email"
                 className="placeholder-gray-400 text-center w-32 h-12 bg-gray-200 rounded-sm"
@@ -59,7 +59,7 @@ const Footer = () => {
                 Subscribe
               </button>
             </div>
-            <h4 className="text-gray-400 font-light text-sm mt-1">
+            <h4 className="text-gray-400 hidden lg:flex font-light text-sm mt-1">
               what a good day :L
             </h4>
           </div>
