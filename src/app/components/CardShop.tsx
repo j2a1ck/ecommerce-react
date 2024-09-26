9f3fc532652a2467a694df262d8322a35eb66902
Jack <jack.70aw@gmail.com>
2024-09-26 11:31:32 +0330

    fix: card shop for mobile view

--- src/app/components/CardShop.tsx
+++ src/app/components/CardShop.tsx
@@ -1,21 +1,24 @@
 import men from "../../public/images/men.png";
 import women from "../../public/images/women.png";
-import accessories from "../../public/images/accessories.png"
-import kids from "../../public/images/kids.png"
+import accessories from "../../public/images/accessories.png";
+import kids from "../../public/images/kids.png";
 const CardShop = () => {
   return (
-    <div className="h-[770px] bg-slate-100 hidden md:flex lg:flex flex-col items-center">
+    <div className="lg:h-[770px] bg-slate-100 flex flex-col items-center">
       <div className="flex items-center flex-col">
         <h1 className="mt-[80px] text-2xl mb-[10px] font-bold">
           EDITOR’S PICK
         </h1>
-        <h2 className="text-gray-600 text-lg font-normal">
-          Problems trying to resolve the conflict between{" "}
+        <h2 className="text-gray-600 text-lg  block lg:hidden font-normal">
+          Problems trying to resolve <span className="hidden" > the conflict between </span>
+          <span className="block lg:hidden justify-center flex">
+            the conflict between
+          </span>
         </h2>
       </div>
-      <div className="flex ">
+      <div className="flex flex-col lg:flex-row">
         <div
-          className="mt-[48px] h-[500px] w-[500px] bg-cover"
+          className="mt-[48px] hidden lg:flex h-[500px]  w-[500px] bg-top  lg:bg-cover"
           style={{ backgroundImage: `url(${men.src})` }}
         >
           <div className="flex justify-start items-end h-[500px] p-5 ">
@@ -35,26 +38,26 @@ const CardShop = () => {
           </div>
         </div>
         <div className="flex flex-col">
-        <div
-          style={{ backgroundImage: `url(${accessories.src})` }}
-          className="bg-cover w-[240px] mt-[48px] h-[242px] ml-[30px] "
-        >
-          <div className="flex justify-start items-end h-[242px] p-5 ">
-            <button className="flex ml-[9px] w-28 justify-center font-medium items-center rounded-lg h-10 bg-white  ">
-              Accessories
-            </button>
+          <div
+            style={{ backgroundImage: `url(${accessories.src})` }}
+            className="bg-cover w-[240px] mt-[48px] h-[242px] ml-[30px] "
+          >
+            <div className="flex justify-start items-end h-[242px] p-5 ">
+              <button className="flex ml-[9px] w-28 justify-center font-medium items-center rounded-lg h-10 bg-white  ">
+                Accessories
+              </button>
+            </div>
           </div>
-        </div>
-        <div
-          style={{ backgroundImage: `url(${kids.src})` }}
-          className="bg-cover w-[240px] mt-[16px] h-[242px] ml-[30px] "
-        >
-          <div className="flex justify-start items-end h-[242px] p-5 ">
-            <button className="flex ml-[9px] w-20 font-medium justify-center items-center rounded-lg h-10 bg-white  ">
-              Kids
-            </button>
+          <div
+            style={{ backgroundImage: `url(${kids.src})` }}
+            className="bg-cover w-[240px] mt-[16px] h-[242px] ml-[30px] "
+          >
+            <div className="flex justify-start items-end h-[242px] p-5 ">
+              <button className="flex ml-[9px] w-20 font-medium justify-center items-center rounded-lg h-10 bg-white  ">
+                Kids
+              </button>
+            </div>
           </div>
-        </div>
         </div>
       </div>
     </div>
