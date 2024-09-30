3a884bb534a70e75f998d5211d06e90c1ed5af8b
Jack <jack.70aw@gmail.com>
2024-09-30 18:34:38 +0330

    Refactor: CardProduct component

--- src/app/components/CardProduct.tsx
+++ src/app/components/CardProduct.tsx
@@ -1,41 +1,50 @@
 import Product from "./Product";
-const CardProduct = () => {
+
+const ProductGrid = () => {
+  const num = [0, 1, 2, 3, 4, 5, 6, 7];
   return (
-    <div>
-      <div className="flex  flex-col">
-        <div className="flex justify-center flex-col items-center mt-[80px] lg:mb-[80px]">
-          <h3 className="text-gray-500 text-lg m-3 font-normal">
-            Featured Products
-          </h3>
-          <h1 className="text-black hidden lg:flex font-medium text-2xl ">
-            BESTSELLER PRODUCTS
-          </h1>
-          <div className="flex flex-col lg:hidden mt-2 mb-2 text-black font-medium text-2xl">
-            <h1>BESTSELLER</h1>
-            <h1>PRODUCTS</h1>
-          </div>
-          <h2 className="text-gray-500 hidden lg:flex text-base mt-3">
-            Problems trying to resolve the conflict between
-          </h2>
-          <div className="flex text-center flex-col lg:hidden text-gray-500">
-            <h2>Problems trying to resolve the</h2>
-            <h2>conflict between</h2>
-          </div>
-        </div>
-        <div className="flex justify-center flex-col lg:flex-row space-x-6">
-          <Product item={0} />
-          <Product item={1} />
-          <Product item={2} />
-          <Product item={3} />
-        </div>
-        <div className="flex justify-center flex-col lg:flex-row space-x-6 lg:mt-[80px] mb-[80px] ">
-          <Product item={4} />
-          <Product item={5} />
-          <Product item={6} />
-          <Product item={7} />
-        </div>
+    <div className="grid grid-cols-1 lg:grid-cols-4 mx-auto gap-x-8 lg:gap-y-[80px] mb-[80px]">
+      {num.map((item, index) => (
+        <Product item={item} key={index} />
+      ))}
+    </div>
+  );
+};
+
+const ProductHighlight = () => {
+  return (
+    <div className="flex justify-center flex-col items-center mt-[80px] lg:mb-[80px]">
+      <p className="text-gray-500 text-lg m-3 font-normal">Featured Products</p>
+      <h1 className="text-black hidden lg:flex font-medium text-2xl ">
+        BESTSELLER PRODUCTS
+      </h1>
+      <div className="flex flex-col lg:hidden mt-2 mb-2 text-black font-medium text-2xl">
+        <h2>
+          BESTSELLER
+          <br />
+          PRODUCTS
+        </h2>
+      </div>
+      <p className="text-gray-500 hidden lg:flex text-base mt-3">
+        Problems trying to resolve the conflict between
+      </p>
+      <div className="flex text-center flex-col lg:hidden text-gray-500">
+        <p>
+          Problems trying to resolve the
+          <br />
+          conflict between
+        </p>
       </div>
     </div>
+  );
+};
+
+const CardProduct = () => {
+  return (
+    <section className="flex flex-col">
+      <ProductHighlight />
+      <ProductGrid />
+    </section>
   );
 };
 
