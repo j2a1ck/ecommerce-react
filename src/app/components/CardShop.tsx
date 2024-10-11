0e42ae519fd0a5c995719e1c34f5669dd5ff1ac5
Jack <jack.70aw@gmail.com>
2024-10-11 19:39:24 +0330

    refactor: CardShop

--- src/app/components/CardShop.tsx
+++ src/app/components/CardShop.tsx
@@ -2,50 +2,46 @@ import men from "../../public/images/men.png";
 import women from "../../public/images/women.png";
 import accessories from "../../public/images/accessories.png";
 import kids from "../../public/images/kids.png";
+import ShopButton from "./ShopButton";
 const CardShop = () => {
   return (
-    <div className="lg:h-[770px] bg-slate-100 flex flex-col items-center">
+    <section className="lg:h-[770px] h-[1330px] bg-slate-100 flex flex-col items-center">
       <div className="flex items-center flex-col">
         <h1 className="mt-[80px] text-2xl mb-[10px] font-bold">
           EDITOR’S PICK
         </h1>
-        <h2 className="text-gray-600 text-lg  block lg:hidden font-normal">
-          Problems trying to resolve <span className="hidden" > the conflict between </span>
-          <span className="block lg:hidden justify-center flex">
+        <div className="lg:flex">
+          <h2 className="text-gray-600 text-lg font-normal">
+            Problems trying to resolve
+          </h2>
+          <span className="hidden text-gray-600 text-lg  lg:flex">
             the conflict between
           </span>
-        </h2>
+        </div>
+        <span className="block text-gray-600 text-lg lg:hidden">
+          the conflict between
+        </span>
       </div>
       <div className="flex flex-col lg:flex-row">
         <div
           className="mt-[48px] hidden lg:flex h-[500px]  w-[500px] bg-top  lg:bg-cover"
           style={{ backgroundImage: `url(${men.src})` }}
         >
-          <div className="flex justify-start items-end h-[500px] p-5 ">
-            <button className="flex ml-[9px] w-20 font-medium justify-center items-center rounded-lg h-10 bg-white  ">
-              Men
-            </button>
-          </div>
+          <ShopButton isLarge={true}>Men</ShopButton>
         </div>
         <div
           style={{ backgroundImage: `url(${women.src})` }}
           className="bg-cover w-[240px] mt-[48px] h-[500px] ml-[30px] "
         >
-          <div className="flex justify-start items-end h-[500px] p-5 ">
-            <button className="flex ml-[9px] w-20 font-medium justify-center items-center rounded-lg h-10 bg-white  ">
-              Women
-            </button>
-          </div>
+          <ShopButton isLarge={true}>Women</ShopButton>
         </div>
         <div className="flex flex-col">
           <div
             style={{ backgroundImage: `url(${accessories.src})` }}
             className="bg-cover w-[240px] mt-[48px] h-[242px] ml-[30px] "
           >
-            <div className="flex justify-start items-end h-[242px] p-5 ">
-              <button className="flex ml-[9px] w-28 justify-center font-medium items-center rounded-lg h-10 bg-white  ">
-                Accessories
-              </button>
+            <div className="flex justify-start items-end h-[242px] p-5 text-xs ">
+              <ShopButton>Accessories</ShopButton>
             </div>
           </div>
           <div
@@ -53,14 +49,12 @@ const CardShop = () => {
             className="bg-cover w-[240px] mt-[16px] h-[242px] ml-[30px] "
           >
             <div className="flex justify-start items-end h-[242px] p-5 ">
-              <button className="flex ml-[9px] w-20 font-medium justify-center items-center rounded-lg h-10 bg-white  ">
-                Kids
-              </button>
+              <ShopButton>Kids</ShopButton>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 export default CardShop;
